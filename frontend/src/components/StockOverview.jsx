2d9702cac80dbd2f7621a30ca699c371700d9cec
MARTINS2309 <martins2309@gmail.com>
2022-08-03 12:38:28 +0100

    cleanup

--- frontend/src/components/StockOverview.jsx
+++ frontend/src/components/StockOverview.jsx
@@ -24,33 +24,6 @@ export function StockOverview() {
         updatedAt: "2022-02-18T10:58:40.866Z",
         publishedAt: "2022-02-18T10:58:40.865Z",
       },
-      {
-        id: 2,
-        name: "Stock Event 2",
-        product_id: 1,
-        quantity: -4,
-        createdAt: "2022-02-18T10:47:49.151Z",
-        updatedAt: "2022-02-18T10:58:40.866Z",
-        publishedAt: "2022-02-18T10:58:40.865Z",
-      },
-      {
-        id: 3,
-        name: "Stock Event 3",
-        product_id: 2,
-        quantity: 2,
-        createdAt: "2022-02-18T10:47:49.151Z",
-        updatedAt: "2022-02-18T10:58:40.866Z",
-        publishedAt: "2022-02-18T10:58:40.865Z",
-      },
-      {
-        id: 4,
-        name: "Stock Event 4",
-        product_id: 3,
-        quantity: 12,
-        createdAt: "2022-02-18T10:47:49.151Z",
-        updatedAt: "2022-02-18T10:58:40.866Z",
-        publishedAt: "2022-02-18T10:58:40.865Z",
-      },
     ],
   });
 
